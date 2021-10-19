844e75133051bb3021e9515eff8f34aaa5d08323
IlyaMarkelov <marckellow@icloud.com>
2021-10-19 17:32:40 +0300

    Создание светофора

--- Traffic Lights/ViewController.swift
+++ Traffic Lights/ViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var yellowLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var actionButton: UIButton!
+    
+    enum Colors {
+        case red, yellow, green
+    }
+    
+    private var currentColor = Colors.red
+    private var activeColorOn = 1
+    private var activeColorOff = 0.3
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        actionButton.layer.cornerRadius = 10
+        
+        redLabel.layer.masksToBounds = true
+        redLabel.layer.cornerRadius = redLabel.frame.height / 2
+        
+        yellowLabel.layer.masksToBounds = true
+        yellowLabel.layer.cornerRadius = yellowLabel.frame.height / 2
+
+        greenLabel.layer.masksToBounds = true
+        greenLabel.layer.cornerRadius = greenLabel.frame.height / 2
     }
 
-
+    @IBAction func moveColor(_ sender: Any) {
+        actionButton.setTitle("NEXT", for: .normal)
+        
+        switch currentColor {
+        case .red:
+            greenLabel.alpha = activeColorOff
+            redLabel.alpha = CGFloat(activeColorOn)
+            currentColor = .yellow
+        case .yellow:
+            redLabel.alpha = activeColorOff
+            yellowLabel.alpha = CGFloat(activeColorOn)
+            currentColor = .green
+        case .green:
+            yellowLabel.alpha = activeColorOff
+            greenLabel.alpha = CGFloat(activeColorOn)
+            currentColor = .red
+        }
+    }
 }
 
